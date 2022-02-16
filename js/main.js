a3734de2512d27c4217f10c2fff66ecfd0a1a2be
PsyManWorld <mhb.borjali@gmail.com>
2022-02-16 22:20:26 +0330

    New Js For Modal Language"

--- js/main.js
+++ js/main.js
@@ -1,25 +1,61 @@
-// Get the modal
-var modal = document.getElementById("myModal");
+var langcounterfa1 = 0;
+var langcounterfa2 = 0;
+var langcounteren1 = 0;
+var langcounteren2 = 0;
+var totalcounter = 0;
 
-// Get the button that opens the modal
-var btn = document.getElementById("myBtn");
+function farsi1() {
+    if (langcounterfa1 % 2 == 0) {
+        document.querySelector(".firsttexterea").setAttribute("id", "fa");
+        document.querySelector(".Farsi1").style.background = "green";
+        document.querySelector(".Farsi2").style.display = "none";
+    } else {
+        document.querySelector(".Farsi1").style.background = "white";
+        document.querySelector(".Farsi2").style.display = "inline";
 
-// Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
-
-// When the user clicks the button, open the modal 
-btn.onclick = function() {
-    modal.style.display = "block";
-}
-
-// When the user clicks on <span> (x), close the modal
-span.onclick = function() {
-    modal.style.display = "none";
-}
-
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-    if (event.target == modal) {
-        modal.style.display = "none";
     }
+    langcounterfa1++;
+    totalcounter++;
+
+}
+
+function farsi2() {
+    if (langcounterfa2 % 2 == 0) {
+        document.querySelector(".secondtexterea").setAttribute("id", "fa");
+        document.querySelector(".Farsi2").style.background = "green";
+        document.querySelector(".Farsi1").style.display = "none";
+    } else {
+        document.querySelector(".Farsi2").style.background = "white";
+        document.querySelector(".Farsi1").style.display = "inline";
+
+    }
+    langcounterfa2++;
+}
+
+function english1() {
+
+    if (langcounterfa1 % 2 == 0) {
+        document.querySelector(".firsttexterea").setAttribute("id", "en");
+        document.querySelector(".English1").style.background = "green";
+        document.querySelector(".English2").style.display = "none";
+    } else {
+        document.querySelector(".English1").style.background = "white";
+        document.querySelector(".English2").style.display = "inline";
+
+    }
+    langcounterfa1++;
+
+}
+
+function english2() {
+    if (langcounteren2 % 2 == 0) {
+        document.querySelector(".secondtexterea").setAttribute("id", "en");
+        document.querySelector(".English2").style.background = "green";
+        document.querySelector(".English1").style.display = "none";
+    } else {
+        document.querySelector(".English2").style.background = "white";
+        document.querySelector(".English1").style.display = "inline";
+
+    }
+    langcounteren2++;
 }
