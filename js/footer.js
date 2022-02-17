683ee19b880d73c0dbe44075d77e9f66bfd8ae53
PsyManWorld <mhb.borjali@gmail.com>
2022-02-17 22:05:56 +0330

    New Changes In JS For DropDown

---
+++ js/footer.js
@@ -0,0 +1,48 @@
+// Have Bug in Changing langselect2
+document.querySelector(".langselect1").value = "fa";
+document.querySelector(".firsttexterea").style.textAlign = "right";
+document.querySelector(".langselect2").value = "en";
+
+function selectlang1() {
+    var x = document.querySelector(".langselect1").value;
+    document.querySelector(".firsttexterea").setAttribute("id", x);
+    switch (x) {
+        case "fa":
+            document.querySelector(".langselect2 .fa").style.display = "none";
+            document.querySelector(".langselect2 .en").style.display = "block";
+            document.querySelector(".langselect2").value = "en";
+            document.querySelector(".firsttexterea").style.textAlign = "right";
+            break;
+
+        case "en":
+            document.querySelector(".langselect2 .fa").style.display = "block";
+            document.querySelector(".langselect2 .en").style.display = "none";
+            document.querySelector(".langselect2").value = "fa";
+            document.querySelector(".firsttexterea").style.textAlign = "left";
+            break;
+        default:
+            //Nothing
+    }
+}
+
+function selectlang2() {
+    var x = document.querySelector(".langselect2").value;
+    document.querySelector(".firsttexterea").setAttribute("id", x);
+    switch (x) {
+        case "fa":
+            document.querySelector(".langselect1 .fa").style.display = "none";
+            document.querySelector(".langselect1 .en").style.display = "block";
+            document.querySelector(".langselect1").value = "en";
+            document.querySelector(".firsttexterea").style.textAlign = "right";
+            break;
+
+        case "en":
+            document.querySelector(".langselect1 .fa").style.display = "block";
+            document.querySelector(".langselect1 .en").style.display = "none";
+            document.querySelector(".langselect1").value = "fa";
+            document.querySelector(".firsttexterea").style.textAlign = "left";
+            break;
+        default:
+            //Nothing
+    }
+}
