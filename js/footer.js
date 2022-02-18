6951b612d44f513a5fb5efd6908c05210470516d
PsyManWorld <mhb.borjali@gmail.com>
2022-02-18 20:54:00 +0330

    The New Change Of Psy Translator

--- js/footer.js
+++ js/footer.js
@@ -12,6 +12,8 @@ function selectlang1() {
             document.querySelector(".langselect2 .en").style.display = "block";
             document.querySelector(".langselect2").value = "en";
             document.querySelector(".firsttexterea").style.textAlign = "right";
+            document.querySelector(".p-firsttexterea").innerHTML = "زبان مبدا: فارسی"
+            document.querySelector(".p-secondtexterea").innerHTML = "زبان مقصد: انگلیسی"
             break;
 
         case "en":
@@ -19,6 +21,9 @@ function selectlang1() {
             document.querySelector(".langselect2 .en").style.display = "none";
             document.querySelector(".langselect2").value = "fa";
             document.querySelector(".firsttexterea").style.textAlign = "left";
+            document.querySelector(".p-firsttexterea").innerHTML = "زبان مبدا: انگلیسی"
+            document.querySelector(".p-secondtexterea").innerHTML = "زبان مقصد: فارسی"
+
             break;
         default:
             //Nothing
@@ -34,6 +39,8 @@ function selectlang2() {
             document.querySelector(".langselect1 .en").style.display = "block";
             document.querySelector(".langselect1").value = "en";
             document.querySelector(".secondtexterea").style.textAlign = "right";
+            document.querySelector(".p-firsttexterea").innerHTML = "زبان مبدا: انگلیسی"
+            document.querySelector(".p-secondtexterea").innerHTML = "زبان مقصد: فارسی"
             break;
 
         case "en":
@@ -41,14 +48,14 @@ function selectlang2() {
             document.querySelector(".langselect1 .en").style.display = "none";
             document.querySelector(".langselect1").value = "fa";
             document.querySelector(".secondtexterea").style.textAlign = "left";
+            document.querySelector(".p-firsttexterea").innerHTML = "زبان مبدا: فارسی"
+            document.querySelector(".p-secondtexterea").innerHTML = "زبان مقصد: انگلیسی"
             break;
         default:
             //Nothing
     }
 }
 
-function textchange()
-{
-document.querySelector(".secondtexterea").value=document.querySelector(".firsttexterea").value;
+function textchange() {
+    document.querySelector(".secondtexterea").value = document.querySelector(".firsttexterea").value;
 }
-
