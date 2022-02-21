ed2c6995b75cf5b58f5144b0c7506fb121a78997
PsyManWorld <mhb.borjali@gmail.com>
2022-02-21 22:17:34 +0330

    The New uncomplite Changes For Parsing Change

--- js/footer.js
+++ js/footer.js
@@ -57,12 +57,48 @@ function selectlang2() {
 }
 
 function textchange() {
-    document.querySelector(".secondtexterea").value = document.querySelector(".firsttexterea").value;
+    document.querySelector(".secondtexterea").textContent = document.querySelector(".firsttexterea").textContent;
+
+
 }
 
 function height() {
+
 
     document.querySelector(".firsttexterea").style.height = "200px"
     document.querySelector(".secondtexterea").style.height = "200px"
 
 }
+
+var ignoreClickOnMeElement = document.getElementById('tex1');
+
+// document.addEventListener('click', function(event) {
+//     var isClickInsideElement = ignoreClickOnMeElement.contains(event.target);
+//     if (!isClickInsideElement) {
+//         var k = 1;
+//         var text = "";
+//         let text1 = document.querySelector(".firsttexterea").textContent;
+
+//         const myArray = text1.split(" ");
+//         myArray.forEach(myFunction);
+
+
+//         function myFunction(items) {
+
+//             let p = document.createElement("span");
+//             let b = document.createElement("span");
+//             p.setAttribute("class", "discritText")
+
+//             p.innerHTML = items
+//             b.innerHTML = " ";
+
+//             document.querySelector(".firsttexterea").append(p);
+//             document.querySelector(".firsttexterea").append(b);
+
+
+
+//         }
+
+//         document.querySelector(".firsttexterea").childNodes[1].innerHTML = "";
+//     }
+// });
